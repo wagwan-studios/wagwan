975985f59d74226abde84372041ab6ba7046b04a
Nabeel Ali Khan <hnabeel64@gmail.com>
2023-11-13 22:16:48 +0500

    Footer Changes

--- src/components/widgets/footer/index.tsx
+++ src/components/widgets/footer/index.tsx
@@ -5,14 +5,14 @@ const Footer = () => {
   return(
       <footer className='min-h-screen bg-tertiary-color p-[30px]'>
           <div className='flex flex-col items-center justify-center pt-10'>
-              <Image src={"/logo.png"} alt={"Wagwan"} width={40} height={40} className="md:w-20 md:h-20 mb-10"/>
+              <Image src={"/logo.png"} alt={"Wagwan"} width={40} height={40} className="mb-10 md:w-20 md:h-20"/>
               <div className='text-center mb-20 md:mb-[150px] lg:max-w-[50%]'>
                   <h4 className='heading-h4 !font-light pb-8 '>
                       Are you a change maker?
                       Work with us.
                   </h4>
-                  <h4 className='heading-h4 !font-light'>Wagwan@gmail.com</h4>
-                  <h4 className='heading-h4 !font-light'>+123-456-7890</h4>
+                  <h4 className='heading-h4 !font-light'>work@wagwanstudios.com</h4>
+                  {/* <h4 className='heading-h4 !font-light'>+123-456-7890</h4> */}
               </div>
               <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
                   <div>
@@ -26,16 +26,16 @@ const Footer = () => {
                   <div>
                       <h5 className="heading-h5 !font-light pb-4">GENERAL ENQUIRES</h5>
                       <ul>
-                          <li className="heading-h6 !font-light pb-3">hello@raggededge.com</li>
+                          <li className="heading-h6 !font-light pb-3">sales@wagwanstudios.com</li>
                           <li className='heading-h6 !font-light '>See job openings </li>
                       </ul>
                   </div>
                   <div>
                       <h5 className="heading-h5 !font-light pb-4">FIND US</h5>
                       <ul>
-                          <li className="heading-h6 !font-light">The Bindery
+                          {/* <li className="heading-h6 !font-light">The Bindery
                               51-53 Hatton Garden
-                              London EC1N 8HN</li>
+                              London EC1N 8HN</li> */}
                       </ul>
                   </div>
                   <div>
